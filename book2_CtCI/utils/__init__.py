5a0dc4f45b8b87ab8196051b074f3332b9f8b4c1
Masao-Taketani <taket003@umn.edu>
2020-07-11 21:34:25 +0900

    1.2 is done and update the test code.

--- book2_CtCI/utils/__init__.py
+++ book2_CtCI/utils/__init__.py
@@ -1,20 +1,33 @@
 def check_true(method, args):
-    result = method(args)
+    result = check_instance_and_return_result(method, args)
     assert result == True, "An error occurred for case {}".format(args)
+
 
 def check_all_true_cases(method, true_cases):
     for t in true_cases:
         check_true(method, t)
     print_success()
 
+
 def check_false(method, args):
-    result = method(args)
+    result = check_instance_and_return_result(method, args)
     assert result == False, "An error occurred for case {}".format(args)
+
 
 def check_all_false_cases(method, false_cases):
     for f in false_cases:
         check_false(method, f)
     print_success()
 
+
 def print_success():
     print("passed the tests.")
+
+
+def check_instance_and_return_result(method, args):
+    # check if t is instance of `int` or `str`
+    if isinstance(args, (int, str)):
+        result = method(args)
+    else:
+        result = method(*args)
+    return result
